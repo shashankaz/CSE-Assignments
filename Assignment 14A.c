e886110f6e48f071ad6e285d5716cad9b9bfaf86
Shashank <113260615+shashankaz@users.noreply.github.com>
2023-02-14 19:59:21 +0530

    Create Assignment 14A.c

---
+++ Assignment 14A.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void swap(int *a, int *b);
+
+int main() 
+{
+    int x, y;
+
+    printf("Enter the First Number : ");
+    scanf("%d", &x);
+
+    printf("Enter the Second Number : ");
+    scanf("%d", &y);
+
+    printf("Before Swapping : x = %d, y = %d\n", x, y);
+
+    swap(&x, &y);
+
+    printf("After Swapping : x = %d, y = %d\n", x, y);
+
+    return 0;
+}
+
+void swap(int *a, int *b) 
+{
+    int temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
